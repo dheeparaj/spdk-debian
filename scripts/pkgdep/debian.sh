bc7442f8ddd1bbb64ac97946994ff38a86bd2c0a
Rajagopalan, Deepa <deepa.rajagopalan@intel.com>
2022-10-18 12:04:51 -0700

    making changes to build openssl from source and install python from source and with ssl support

--- scripts/pkgdep/debian.sh
+++ scripts/pkgdep/debian.sh
@@ -3,27 +3,32 @@
 VERSION_ID_NUM=$(sed 's/\.//g' <<< $VERSION_ID)
 # Includes Ubuntu, Debian
 # Minimal install
-apt-get install -y gcc g++ make libcunit1-dev libaio-dev libssl-dev libjson-c-dev libcmocka-dev uuid-dev libiscsi-dev
-if [[ $NAME == "Ubuntu" ]] && [[ $VERSION_ID_NUM -ge 2204 ]]; then
-	# there is no python package in Ubuntu 22.04
-	apt-get install -y python3
-else
-	apt-get install -y python
-fi
-apt-get install -y libncurses5-dev libncursesw5-dev python3-pip
-pip3 install ninja
-if ! pip3 install meson; then
+export DEBIAN_FRONTEND="noninteractive" 
+export TZ="America/New_York" 
+export https_proxy=http://proxy-dmz.intel.com:912
+export http_proxy=http://proxy-dmz.intel.com:911
+apt-get install -y gcc g++ make cmake libcunit1-dev libaio-dev libjson-c-dev libcmocka-dev \
+	uuid-dev libiscsi-dev libncurses5-dev libncursesw5-dev \
+        libreadline-gplv2-dev  libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev libssl-dev
+cd /src
+wget -O - https://www.openssl.org/source/openssl-1.1.1g.tar.gz | tar zxf - && cd openssl-1.1.1g && ./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl no-ssl2 && make -j4 && make install 
+export PATH=/usr/local/ssl/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/ssl/lib
+export LC_ALL="en_US.UTF-8"
+export LDFLAGS="-L /usr/local/ssl/lib -Wl,-rpath,/usr/local/ssl/lib"
+cd /src
+wget --no-check-certificate -O - https://www.python.org/ftp/python/3.8.0/Python-3.8.0.tgz | tar xzf - && cd Python-3.8.0 && sed -i '/ssl/s/^#//g' Modules/Setup && ./configure --with-openssl=/usr/local/ssl && make -j4 && make install 
+pip3 install --upgrade pip
+pip3 install --proxy http://proxy-dmz.intel.com:912  ninja
+if ! pip3 install --proxy http://proxy-dmz.intel.com:912 meson; then
 	# After recent updates pip3 on ubuntu1604 provides meson version which requires python >= 3.6.
 	# Unfortunately, the latest available version of python3 there is 3.5.2. In case pip3 fails to
 	# install meson fallback to packaged version of it ubuntu1604's repos may provide.
 	apt-get install -y meson
 fi
-pip3 install pyelftools
-pip3 install ijson
-pip3 install python-magic
-pip3 install grpcio
-pip3 install grpcio-tools
-pip3 install pyyaml
+pip3 install --proxy http://proxy-dmz.intel.com:912 pyelftools
+pip3 install --proxy http://proxy-dmz.intel.com:912 ijson
+pip3 install --proxy http://proxy-dmz.intel.com:912 python-magic
 # Additional dependencies for SPDK CLI - not available on older Ubuntus
 apt-get install -y python3-configshell-fb python3-pexpect || echo \
 	"Note: Some SPDK CLI dependencies could not be installed."
